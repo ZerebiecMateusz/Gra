42f74aa6f3f9f9c26e34f71cc07847f48ac042b8
Mateusz Zerebiec <43859749+ZerebiecMateusz@users.noreply.github.com>
2023-09-29 14:10:44 +0200

    game works

--- app/Game.js
+++ app/Game.js
@@ -1,39 +1,105 @@
-import {Card} from './Card.js';
+import { Player } from './Player.js';
 import { Deck } from './Deck.js';
-import { Player} from './Player.js';
+import { Table } from './Table.js';
+import { Message } from './Message.js';
 
 class Game {
-    constructor({playersCards, dealersCards, player}) {
-        this.player = player;
-        this.dealer = new Player('krupier');
-        this.playersCards = playersCards;
-        this.dealersCards = dealersCards;
-        this.deck = new Deck();
-        this.deck.shuffle();
-    }
-    run() {
-        this.dealCards();
-    };
+	constructor({ player, playerPoints, dealerPoints, table, hitButton, standButton, messageBox }) {
+		this.hitButton = hitButton;
+		this.standButton = standButton;
+		this.playerPoints = playerPoints;
+		this.dealerPoints = dealerPoints;
+		this.messageBox = messageBox;
+		this.player = player;
+		this.dealer = new Player('Krupier');
+		this.table = table;
+		this.deck = new Deck();
+		this.deck.shuffle();
+	}
 
-    dealCards() {
-        for (let n = 0; n < 2; n++) {
-            let card1 = this.deck.pickOne();
-            this.player.hand.addCard(card1);
-            this.playersCards.appendChild(card1.render());
+	run() {
+		this.hitButton.addEventListener('click', (event) => this.hitCard());
+		this.standButton.addEventListener('click', (event) => this.dealerPlays());
+		this.dealCards();
+	}
 
-            let card2 = this.deck.pickOne();
-            this.dealer.hand.addCard(card2);
-            this.dealersCards.appendChild(card2.render());
+	hitCard() {
+		const card = this.deck.pickOne();
+		this.player.hand.addCard(card);
+		this.table.showPlayersCard(card);
+		this.playerPoints.innerHTML = this.player.calculatePoints();
+	}
 
-        }
-    }
+	dealCards() {
+		for (let n = 0; n < 2; n++) {
+			let card1 = this.deck.pickOne();
+			this.player.hand.addCard(card1);
+			this.table.showPlayersCard(card1);
+
+			let card2 = this.deck.pickOne();
+			this.dealer.hand.addCard(card2);
+			this.table.showDealersCard(card2);
+		}
+
+		this.playerPoints.innerHTML = this.player.calculatePoints();
+		this.dealerPoints.innerHTML = this.dealer.calculatePoints();
+	}
+
+	dealerPlays() {
+		while (this.dealer.points <= this.player.points && this.dealer.points <= 21 && this.player.points <= 21) {
+			const card = this.deck.pickOne();
+			this.dealer.hand.addCard(card);
+			this.table.showDealersCard(card);
+			this.dealerPoints.innerHTML = this.dealer.calculatePoints();
+		}
+
+		this.endTheGame();
+	}
+
+	endTheGame() {
+		this.hitButton.removeEventListener('click', (event) => this.hitCard());
+		this.standButton.removeEventListener('click', (event) => this.dealerPlays());
+
+		this.hitButton.style.display = 'none';
+		this.standButton.style.display = 'none';
+
+		if (this.player.points < 21 && this.player.points == this.dealer.points) {
+			this.messageBox.setText('Remis').show();
+
+			return;
+		}
+
+		if (this.player.points > 21) {
+			this.messageBox.setText('Wygrywa Dealer').show();
+			return;
+		}
+
+		if (this.dealer.points > 21) {
+			this.messageBox.setText('Wygrywa Player').show();
+
+			return;
+		}
+
+		if (this.player.points < this.dealer.points) {
+			this.messageBox.setText('Wygrywa Dealer').show();
+
+			return;
+		}
+	}
 }
 
-const player = new Player('Mateusz');
+const table = new Table(document.getElementById('dealersCards'), document.getElementById('playersCards'));
+const messageBox = new Message(document.getElementById('message'));
+
+const player = new Player('Kacper');
 const game = new Game({
-    player,
-    playersCards: document.getElementById('playersCards'),
-    dealersCards: document.getElementById('dealersCards')
-})
+	hitButton: document.getElementById('hit'),
+	standButton: document.getElementById('stand'),
+	dealerPoints: document.getElementById('dealerPoints'),
+	playerPoints: document.getElementById('playerPoints'),
+	player,
+	table,
+	messageBox
+});
 
 game.run();
---
+++ app/Table.js
@@ -0,0 +1,16 @@
+import { Hand } from "./Hand.js";
+
+export class Table {
+	constructor(dealersCards, playersCards) {
+		this.dealersCards = dealersCards;
+		this.playersCards = playersCards;
+	}
+
+	showPlayersCard(card) {
+		this.playersCards.appendChild(card.render());
+	}
+
+	showDealersCard(card) {
+		this.dealersCards.appendChild(card.render());
+	}
+}
